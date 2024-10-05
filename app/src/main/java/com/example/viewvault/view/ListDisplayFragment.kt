738f78bd204d50f1ae4bd3cf9f68d38c0b3e8b3c
AnthonyDLC <arod2009417@gmail.com>
2024-10-05 16:40:49 -0700

    Created ListDisplayFragment and implemented it into main activity

--- app/src/main/java/com/example/viewvault/view/ListDisplayFragment.kt
+++ app/src/main/java/com/example/viewvault/view/ListDisplayFragment.kt
@@ -1,4 +1,27 @@
 package com.example.viewvault.view
 
-class ListDisplayFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.viewvault.databinding.ListDisplayFragmentBinding
+
+class ListDisplayFragment : Fragment() {
+    private lateinit var binding : ListDisplayFragmentBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ListDisplayFragmentBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
